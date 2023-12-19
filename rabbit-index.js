527b2893d67ed2259464f18ff476e33e1b4627eb
aguspray001 <aguspray001@gmail.com>
2023-12-20 06:18:20 +0700

    feat: add rabbitmq

--- rabbit-index.js
+++ rabbit-index.js
@@ -1,71 +1,9 @@
+const amqplib = require("amqplib");
+// 
 const { WebSocket } = require("ws");
-const { Kafka, logLevel } = require("kafkajs");
 const { createHistory } = require("./src/controller/homeAssistant");
 const { performance } = require("perf_hooks");
 require("dotenv").config();
-
-const topic = "blockchain-connector";
-const kafka = new Kafka({
-  clientId: "my-broker",
-  brokers: [`${process.env.KAFKA_URI}:${process.env.KAFKA_PORT}`],
-  logLevel: logLevel.NOTHING,
-});
-let isProcessing = false;
-
-const produceToKafka = async (data) => {
-  // kafka producer send to kafka broker
-  const producer = kafka.producer();
-  await producer.connect();
-  await producer.send({
-    topic: topic,
-    messages: [{ value: JSON.stringify(data) }],
-  });
-  await producer.disconnect();
-};
-const data = [];
-const consumeToKafka = async () => {
-  const consumer = kafka.consumer({ groupId: "blockchain-connector" });
-  await consumer.connect();
-  await consumer.subscribe({ topics: [topic], fromBeginning: false });
-  // let response = null
-  await consumer.run({
-    autoCommit: false,
-    eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
-      try {
-        // console.log(message)
-        const value = message.value.toString();
-        // console.log(`Received message on topic ${topic}, partition ${partition}: ${value}`);
-        var startTime = performance.now();
-        await consumer.pause([{ topic }])    
-        const history = await createHistory(privateKey, value);
-        data.push(history)
-        // console.log(`percobaan ke-${data.length}`)
-        await consumer.resume([{ topic }])
-        await consumer.commitOffsets([{ topic, partition, offset: message.offset }])
-        // console.log("consume to sc => ",history)
-        var endTime = performance.now();
-        console.log(`${data.length} | ${endTime - startTime}`);
-        return value;
-        // Perform your custom logic with the received value
-      } catch (error) {
-        console.error('Error processing message:', error);
-      }
-    },
-  });
-  // await consumer.disconnect();
-  // return response;
-};
-
-const normalProduce = async (privateKey, data) => {
-  if(!isProcessing){
-    isProcessing = true;
-    var startTime = performance.now();
-    const history = await createHistory(privateKey, data);
-    var endTime = performance.now();
-    console.log(`${data.length} | ${endTime - startTime}`);
-    isProcessing = false;
-  }
-}
 
 const port = process.env.WEBSOCKET_PORT;
 const uri = process.env.WEBSOCKET_URI;
@@ -84,6 +22,42 @@ const subsEvent = {
   type: "subscribe_events",
   event_type: "state_changed",
 };
+
+// rabbitmq config
+const queueTopic = "iot-data";
+
+const rabbitConnect = async () => {
+  conn = await amqplib.connect("amqp://10.0.2.15:5672");
+  return conn;
+}
+
+const rabbitSend = async (conn, topic, data) => {
+  // producer
+  const buffData = Buffer.from(JSON.stringify(data));
+  const ch = await conn.createChannel();
+  ch.sendToQueue(topic, buffData);
+};
+
+const rabbitConsume = async (conn, topic, privateKey) => {
+    // consumer
+    const ch = await conn.createChannel();
+    await ch.assertQueue(topic);
+    ch.consume(topic, async (msg) => {
+      if (msg !== null) {
+        let respData = msg.content.toString();
+        // console.log("rabbit consume => ", respData);
+        ch.ack(msg);
+  
+        var startTime = performance.now();
+        const history = await createHistory(privateKey, respData);
+        // console.log("consume to sc => ",history)
+        var endTime = performance.now();
+        console.log(`${endTime - startTime}`);
+      } else {
+        console.log("consumer cancelled by server");
+      }
+    });
+}
 
 socket.addEventListener("open", () => {
   // send a message to the server
@@ -121,14 +95,11 @@ socket.addEventListener("message", async ({ data }) => {
 
     privateKey = "0x" + process.env.PRIVATE_KEY;
 
-    // normal testing
-    // normalProduce(privateKey, response);
-
     // produce data to kafka
-    // console.log("tes!!!")
-    await produceToKafka(response);
-    console.log(response);
+    const connRabbit = await rabbitConnect();
+    await rabbitSend(connRabbit, queueTopic, response);
+    // console.log(response);
     // get data from kafka
-    await consumeToKafka();
+    await rabbitConsume(connRabbit, queueTopic, privateKey);
   }
 });
